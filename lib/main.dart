455930dd7906d9852fb8edc298ca7212ca343091
Jpbroadvision <jpbroadvision2@gmail.com>
2022-01-06 19:51:33 +0000

    chore: Initial setup

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flut_quiz_app/quiz_home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(QuizHome());
+}
---
+++ lib/quiz_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QuizHome extends StatefulWidget {
+  const QuizHome({ Key? key }) : super(key: key);
+
+  @override
+  _QuizHomeState createState() => _QuizHomeState();
+}
+
+class _QuizHomeState extends State<QuizHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
