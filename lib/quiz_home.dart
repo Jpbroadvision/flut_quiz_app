74d523de30e01d04796f32a87dabe8a2112406de
Jpbroadvision <jpbroadvision2@gmail.com>
2022-01-10 05:50:50 +0000

    chore: Change title and add bg-color

--- lib/quiz_home.dart
+++ lib/quiz_home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class QuizHome extends StatefulWidget {
-  const QuizHome({ Key? key }) : super(key: key);
+  const QuizHome({Key? key}) : super(key: key);
+
+  // const QuizHome({ Key? key }) : super(key: key);
 
   @override
   _QuizHomeState createState() => _QuizHomeState();
@@ -11,7 +13,11 @@ class _QuizHomeState extends State<QuizHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("True Citizen"),),
+      appBar: AppBar(
+        title: const Text("True Citizen"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
     );
   }
 }
